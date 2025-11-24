195d90ec563244381ea79f77a0ee0c45d6621068
Frozt98 <andres10sala98@gmail.com>
2025-11-23 21:04:39 -0500

    Inicializando la clase ArchivoAdjunto

--- src/main/java/unl/edu/cc/workunity/domain/ArchivoAdjunto.java
+++ src/main/java/unl/edu/cc/workunity/domain/ArchivoAdjunto.java
@@ -1,5 +1,55 @@
 package unl.edu.cc.workunity.domain;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.StringJoiner;
+import unl.edu.cc.workunity.domain.enums.TipoArchivo;
+
 public class ArchivoAdjunto {
+  private float tamanio;
+  private byte[] contenido;
+  private TipoArchivo tipoArchivo;
+
+    public ArchivoAdjunto() {
+    }
+
+    public ArchivoAdjunto(float tamanio, byte[] contenido, TipoArchivo tipoArchivo) {
+        this.tamanio = tamanio;
+        this.contenido = contenido;
+        this.tipoArchivo = tipoArchivo;
+    }
+
+    public float getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(float tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public byte[] getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(byte[] contenido) {
+        this.contenido = contenido;
+    }
+
+    public TipoArchivo getTipoArchivo() {
+        return tipoArchivo;
+    }
+
+    public void setTipoArchivo(TipoArchivo tipoArchivo) {
+        this.tipoArchivo = tipoArchivo;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ArchivoAdjunto.class.getSimpleName() + "[", "]")
+                .add("tamanio=" + tamanio)
+                .add("contenido=" + Arrays.toString(contenido))
+                .add("tipoArchivo=" + tipoArchivo)
+                .toString();
+    }
 
 }
