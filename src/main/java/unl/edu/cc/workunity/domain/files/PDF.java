ff3a33864c8188b3fcb6df3dfe7e0c4d66612e30
Frozt98 <andres10sala98@gmail.com>
2025-11-27 18:58:07 -0500

    Agregando metodo validarTamaño de la clase padre ArchivoAdjunto

--- src/main/java/unl/edu/cc/workunity/domain/files/PDF.java
+++ src/main/java/unl/edu/cc/workunity/domain/files/PDF.java
@@ -1,4 +1,11 @@
 package unl.edu.cc.workunity.domain.files;
 
-public class PDF {
-}
+import unl.edu.cc.workunity.domain.ArchivoAdjunto;
+
+public class PDF extends ArchivoAdjunto {
+    public PDF(byte[] contenido) {
+        super(contenido);
+    }
+    }
+
+
