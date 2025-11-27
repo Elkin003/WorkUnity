72df23d32ce424ecf4071a6ff50b60251d9c903b
Frozt98 <andres10sala98@gmail.com>
2025-11-27 18:57:44 -0500

    Agregando metodo validarTamaño de la clase padre ArchivoAdjunto

--- src/main/java/unl/edu/cc/workunity/domain/files/Imagen.java
+++ src/main/java/unl/edu/cc/workunity/domain/files/Imagen.java
@@ -1,4 +1,14 @@
 package unl.edu.cc.workunity.domain.files;
 
-public class Imagen {
+import unl.edu.cc.workunity.domain.ArchivoAdjunto;
+
+public class Imagen extends ArchivoAdjunto {
+    public enum TipoImagen { PNG, JPG }
+
+    private TipoImagen tipo;
+
+    public Imagen(byte[] contenido, TipoImagen tipo) {
+        super(contenido);
+        this.tipo = tipo;
+    }
 }
