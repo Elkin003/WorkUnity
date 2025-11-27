98b7a472d5936dcddaca0ae90d2083d62a4a323d
Frozt98 <andres10sala98@gmail.com>
2025-11-27 18:58:31 -0500

    Agregando metodo validarTamaño de la clase padre ArchivoAdjunto

--- src/main/java/unl/edu/cc/workunity/domain/files/Word.java
+++ src/main/java/unl/edu/cc/workunity/domain/files/Word.java
@@ -1,4 +1,9 @@
 package unl.edu.cc.workunity.domain.files;
 
-public class Word {
+import unl.edu.cc.workunity.domain.ArchivoAdjunto;
+
+public class Word extends ArchivoAdjunto {
+    public Word(byte[] contenido) {
+        super(contenido);
+    }
 }
