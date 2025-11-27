464b702d999821c3941c71c51cd239009a3fa927
Frozt98 <andres10sala98@gmail.com>
2025-11-27 18:52:51 -0500

    Agregando metodos con excepciones

--- src/main/java/unl/edu/cc/workunity/domain/ArchivoAdjunto.java
+++ src/main/java/unl/edu/cc/workunity/domain/ArchivoAdjunto.java
@@ -1,23 +1,28 @@
 package unl.edu.cc.workunity.domain;
 
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.StringJoiner;
-import unl.edu.cc.workunity.domain.enums.TipoArchivo;
 
-public class ArchivoAdjunto {
-  private float tamanio;
-  private byte[] contenido;
-  private TipoArchivo tipoArchivo;
+public abstract class ArchivoAdjunto {
+  protected float tamanio;
+  protected byte[] contenido;
 
-    public ArchivoAdjunto() {
-    }
+    // Tamaño máximo permitido para el archivo 20 MB convertidos a bytes
+    private static final int TamanioMaximoBytes = 20 * 1024 * 1024;
 
-    public ArchivoAdjunto(float tamanio, byte[] contenido, TipoArchivo tipoArchivo) {
+    public ArchivoAdjunto(byte[] contenido) {
         this.tamanio = tamanio;
         this.contenido = contenido;
-        this.tipoArchivo = tipoArchivo;
+       validarTamanio();
     }
+
+    public void validarTamanio() {
+        if (contenido.length > TamanioMaximoBytes) {
+            throw new IllegalArgumentException("El archivo supera los 20MB permitidos");
+        }
+    }
+
+
 
     public float getTamanio() {
         return tamanio;
@@ -35,21 +40,11 @@ public class ArchivoAdjunto {
         this.contenido = contenido;
     }
 
-    public TipoArchivo getTipoArchivo() {
-        return tipoArchivo;
-    }
-
-    public void setTipoArchivo(TipoArchivo tipoArchivo) {
-        this.tipoArchivo = tipoArchivo;
-    }
-
     @Override
     public String toString() {
         return new StringJoiner(", ", ArchivoAdjunto.class.getSimpleName() + "[", "]")
                 .add("tamanio=" + tamanio)
                 .add("contenido=" + Arrays.toString(contenido))
-                .add("tipoArchivo=" + tipoArchivo)
                 .toString();
     }
-
 }
