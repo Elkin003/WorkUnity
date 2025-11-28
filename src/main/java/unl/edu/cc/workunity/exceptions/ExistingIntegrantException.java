5362856693ca0d495eaf09d6e11b9a6e68455d07
Elkin003 <elkinjjm@gmail.com>
2025-11-27 23:09:04 -0500

    Creación de una excepción para cuando ya existe un integrante en un proyecto

---
+++ src/main/java/unl/edu/cc/workunity/exceptions/ExistingIntegrantException.java
@@ -0,0 +1,7 @@
+package unl.edu.cc.workunity.exceptions;
+
+public class ExistingIntegrantException extends RuntimeException {
+    public ExistingIntegrantException(String message) {
+        super(message);
+    }
+}
