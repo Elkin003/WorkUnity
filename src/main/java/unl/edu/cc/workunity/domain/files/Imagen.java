23335eefedc3e9fe89ae189194a70ec2f3e32047
Frozt98 <andres10sala98@gmail.com>
2025-11-27 20:51:27 -0500

    Mejorando el metodo

--- src/main/java/unl/edu/cc/workunity/domain/files/Imagen.java
+++ src/main/java/unl/edu/cc/workunity/domain/files/Imagen.java
@@ -1,14 +1,22 @@
 package unl.edu.cc.workunity.domain.files;
 
 import unl.edu.cc.workunity.domain.ArchivoAdjunto;
+import unl.edu.cc.workunity.domain.enums.TipoImagen;
 
 public class Imagen extends ArchivoAdjunto {
-    public enum TipoImagen { PNG, JPG }
 
     private TipoImagen tipo;
 
     public Imagen(byte[] contenido, TipoImagen tipo) {
         super(contenido);
+        this.tipo = tipo;
+    }
+
+    public TipoImagen getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoImagen tipo) {
         this.tipo = tipo;
     }
 }
